b38cc07707c094445f1bf1e01503103ee28cf110
GW McElfresh <mcelfreshgw@gmail.com>
2024-08-28 14:52:36 -0700

    R package :tada:

---
+++ R/flat2ometiff.R
@@ -0,0 +1,13 @@
+#' @title Init
+#' 
+#' @description
+#' Temporary function to set up R package
+#' @export
+
+Init <- function(echo = T) {
+  if (echo) {
+    print("hello!")
+  } else {
+    print("well then...")
+  }
+}
